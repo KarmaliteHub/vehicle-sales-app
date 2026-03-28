53420113187c91669383e4e0af75ab39d60cd7af
KarmaliteHub <litegithubkarma@gmail.com>
2026-03-28 08:42:04 -0700

    guardar

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,6 +9,28 @@ from dotenv import load_dotenv
 from datetime import timedelta
 from pathlib import Path
 
+if not DEBUG:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+            },
+        },
+        'root': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['console'],
+                'level': 'INFO',
+                'propagate': False,
+            },
+        },
+    }
+
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
